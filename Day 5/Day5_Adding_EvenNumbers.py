59940523c1e2d298ea24db6a76285cfb7acd3656
deepideva <ganesh.learner.2023@gmail.com>
2024-08-04 10:21:11 +0530

    Day5 Learning

--- Day 5/Day5_Adding_EvenNumbers.py
+++ Day 5/Day5_Adding_EvenNumbers.py
@@ -1,4 +1,4 @@
-target=int(input("Enter Number between 0 and 1000 :"))
+target=int(input("Enter Number between 0 and 100 :"))
 '''
 even_sum=0
 for number in range (2, target+1, 2):
@@ -9,9 +9,15 @@ print(even_sum)
 
 
 # Another method
-even_sum=0
+'''even_sum=0
 for number in range (1, target+1): 
     if number % 2 ==0:
         even_sum += number    
-print(even_sum)
+print(even_sum)'''
+
+#Try
+even=0
+for num in range(2, target + 1,2):
+    even+= num
+print(even)
 
